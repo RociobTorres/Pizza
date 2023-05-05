3926a8d71a0da5bd6962b13617cd462b79669887
Rocio <phrociotorres@gmail.com>
2023-05-05 00:06:05 -0300

    Página de Pizzas

---
+++ js/index.js
@@ -0,0 +1,17 @@
+
+
+let nombre = prompt("Escribe tu nombre")
+
+let edad = prompt("Escribe tu edad")
+
+let edadFutura = parseInt(edad) + 2
+
+console.log(`En dos años: ${nombre} va a tener ${edadFutura} años`)
+
+
+let n1 = 1
+let n2 = 5
+
+resultado = n1 + n2
+
+console.log(resultado)
